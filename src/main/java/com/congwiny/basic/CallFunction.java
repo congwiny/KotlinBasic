3fd8e48dc86822ee1425687b8375355aa0a86c44
congwiny <congwiny@newborn-town.com>
2017-08-29 16:12:20 +0800

    Add:调用函数时，使用函数命名参数；声明函数时，使用默认参数值

---
+++ src/main/java/com/congwiny/basic/CallFunction.java
@@ -0,0 +1,46 @@
+package com.congwiny.basic;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
+public class CallFunction {
+
+    public static void main(String[] args) {
+        //FunctionUsingKt.joinToString()
+
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+
+        System.out.println(arrayList);
+
+        String str = StringFunctions.joinToString(arrayList, "; ", "(", ")");
+        System.out.println(str);
+        //编译错误
+        //FunctionUsingKt.joinToString(arrayList, separator = "; ", prefix = "(", postfix = ")");
+
+        //调用Kotlin的提供默认参数值的函数
+        System.out.println(StringFunctions.joinToString2(arrayList));
+    }
+
+    public static <T> String joinToString(Collection<T> collection,
+                                          String separator,
+                                          String prefix,
+                                          String postfix) {
+        StringBuilder result = new StringBuilder(prefix);
+
+        Iterator it = collection.iterator();
+
+        for (int index = 0; index < collection.size(); index++) {
+            if (index > 0) {
+                result.append(separator);
+            }
+            result.append(it.next());
+        }
+        result.append(postfix);
+        return result.toString();
+    }
+}
---
+++ src/main/kotlin/com/congwiny/basic/FunctionUsing.kt
@@ -0,0 +1,66 @@
+/**
+ * 使用@file:JvmName注解，修改文件类名
+ * 必须将这句话放到文件的开头
+ */
+@file:JvmName("StringFunctions")
+
+package com.congwiny.basic
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(list)
+    //在编译器里能看到参数的名字，但是在源文件中无法看到对应的参数名是什么
+    println(joinToString(list, "; ", "(", ")"))
+
+    //使用命名参数（不去查看函数的声明就知道对应的参数）
+    println(joinToString(list, separator = "; ", prefix = "(", postfix = ")"))
+
+
+    println(CallFunction.joinToString(list, "; ", "(", ")"))
+    //named arguments are not allowed for non-Kotlin functions
+    //println(CallFunction.joinToString(list, separator = "; ", prefix = "(", postfix = ")"))
+
+    //使用函数的默认参数值
+    println(joinToString2(list))
+    println(joinToString2(list,"; "))
+    println(joinToString2(list,prefix = "(",postfix = ")"))
+}
+
+
+fun <T> joinToString(collection: Collection<T>,
+                     separator: String,
+                     prefix: String,
+                     postfix: String): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+
+/**
+ * 指定默认参数值的函数
+ *
+ * 在Java中调用Kotlin方法时，由于Java没有参数默认值的概念，
+ * 使用@JvmOverloads注解，让编译器生成多个Java重载函数。
+ * String joinToString(Collection<T> collection, String separator, String prefix, String postfix);
+ * String joinToString(Collection<T> collection, String separator, String prefix);
+ * String joinToString(Collection<T> collection, String separator);
+ * String joinToString(Collection<T> collection);
+ */
+@JvmOverloads
+fun <T> joinToString2(collection: Collection<T>,
+                     separator: String = ", ",
+                     prefix: String = "",
+                     postfix: String = ""): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
